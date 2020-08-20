6fe51e75d05dc6128aa5b270565a25f8265887bc
LucasLN <lucaslimaneves1@hotmail.com>
2020-08-20 17:30:54 -0300

    criado um atributo de classe
    Signed-off-by: LucasLN <lucaslimaneves1@hotmail.com>

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos=2
     def __init__(self,*filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -16,12 +17,19 @@ if __name__ == '__main__':
     renato=Pessoa(lucas,nome = 'Renato', idade='43')
 
     print(renato.cumprimentar())
-    print(renato.nome, renato.idade)
+    print(renato.nome, renato.idade, renato.olhos)
     renato.sobrenome='nogueira'
     for filho in renato.filhos:
         print(filho.nome)
     del renato.filhos
+    renato.olhos=1
+    print(renato.olhos, lucas.olhos)
+    Pessoa.olhos=3
+    print(renato.olhos, lucas.olhos)
+    del renato.olhos
+    print(renato.olhos)
     print(renato.__dict__)
     print(lucas.__dict__)
+
 
 
