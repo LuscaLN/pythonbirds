959dba327630a71b8ceb428224523e8ac6929ebf
LucasLN <lucaslimaneves1@hotmail.com>
2020-08-20 15:55:23 -0300

    criado um atributo dinâmico

---
+++ oo/pessoa.py
@@ -0,0 +1,24 @@
+class Pessoa:
+    def __init__(self,*filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+
+if __name__ == '__main__':
+
+    lucas=Pessoa(nome='Lucas')
+    renato=Pessoa(lucas,nome = 'Renato', idade='43')
+
+    print(renato.cumprimentar())
+    print(renato.nome, renato.idade)
+    renato.sobrenome='nogueira'
+    for filho in renato.filhos:
+        print(filho.nome)
+    del renato.filhos
+    print(renato.__dict__)
