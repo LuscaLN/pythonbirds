badcc47b2c540e8bbeb39b74ad793b3bc44e26b4
LucasLN <lucaslimaneves1@hotmail.com>
2020-08-22 13:45:51 -0300

    Exercício carro completo
    Signed-off-by: LucasLN <lucaslimaneves1@hotmail.com>

--- oo/exercicio_carro.py
+++ oo/exercicio_carro.py
@@ -1,67 +1,63 @@
 
-class carro:
-    class Motor:
-        def __init__(self, velocidade=0):
-            self.velocidade = velocidade
+class Carro:
+    def __init__(self, motor, direcao):
+        self.motor = motor
+        self.direcao = direcao
+    def calc_vel(self):
+        return self.motor.velocidade
+    def acelerar(self):
+        self.motor.acelerar()
+    def frear(self):
+        self.motor.frear()
+    def sentido(self):
+        return self.direcao.rumo[self.direcao.cont]
+    def direita(self):
+        self.direcao.virar_a_direita()
+    def esquerda(self):
+        self.direcao.virar_a_esquerda()
 
-        def acelerar(self):
-             self.velocidade = self.velocidade+1
-        def frear(self):
-            if(self.velocidade>=2):
-                self.velocidade=self.velocidade-2
-            else:
-                self.velocidade=0
+class Motor:
+    def __init__(self, velocidade=0):
+        self.velocidade = velocidade
 
-
-    motor = Motor(velocidade=0)
-
-    class Direcao:
-        def __init__(self, rumo=['Norte', 'Leste', 'Sul', 'Oeste'], cont=0):
-            self.rumo = rumo
-            self.cont = cont
-        def virar_a_direita(self):
-            if self.cont < 3:
-                self.cont = self.cont+1
-            else:
-                self.cont = 0
-        def virar_a_esquerda(self):
-            if self.cont == 0:
-                self.cont = 3
-            else:
-                self.cont = self.cont-1
+    def acelerar(self):
+        self.velocidade = self.velocidade+1
+    def frear(self):
+        if(self.velocidade>=2):
+            self.velocidade=self.velocidade-2
+        else:
+            self.velocidade=0
 
 
+motor = Motor(velocidade=0)
 
-    direcao = Direcao()
+class Direcao:
+    def __init__(self, rumo=['Norte', 'Leste', 'Sul', 'Oeste'], cont=0):
+        self.rumo = rumo
+        self.cont = cont
+    def virar_a_direita(self):
+        if self.cont < 3:
+            self.cont = self.cont+1
+        else:
+            self.cont = 0
+    def virar_a_esquerda(self):
+        if self.cont == 0:
+            self.cont = 3
+        else:
+            self.cont = self.cont-1
 
-    if __name__ == '__main__':
-        print(motor.velocidade)
-        motor.acelerar()
-        print(motor.velocidade)
-        motor.acelerar()
-        motor.acelerar()
-        print(motor.velocidade)
-        motor.frear()
-        print(motor.velocidade)
-        motor.frear()
-        print(motor.velocidade)
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_direita()
-        direcao.virar_a_direita()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_direita()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_direita()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_esquerda()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_esquerda()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_esquerda()
-        print(direcao.rumo[direcao.cont])
-        direcao.virar_a_esquerda()
-        print(direcao.rumo[direcao.cont])
 
+
+direcao = Direcao()
+carro = Carro(motor, direcao)
+
+if __name__ == '__main__':
+    print(carro.calc_vel())
+    carro.acelerar()
+    carro.acelerar()
+    carro.acelerar()
+    print(carro.calc_vel())
+    print(carro.sentido())
 
 
 
